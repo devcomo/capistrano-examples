5ebf46fc51f285f4e128a295a0f9db4ee37a2cea
James R. Carr <james.r.carr@gmail.com>
2012-07-18 14:04:38 -0500

    added a post-deploy hook

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,8 @@ set :user, 'www-deploy'
 set :deploy_to,   "/var/www/"
 set :deploy_via, :remote_cache
 set :use_sudo, false
-set :branch, 'static-site-simple'
+set :branch, 'post-deploy-hook'
+set :twitterUser, 'jamescarr'
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 ssh_options[:forward_agent] = true
@@ -16,3 +17,11 @@ ssh_options[:forward_agent] = true
 role :web, site                      # Your HTTP server, Apache/etc
 role :app, site                      # Your HTTP server, Apache/etc
 
+
+namespace twitter do
+  task user_image do
+    run "wget /users/profile_image/#{twitterUser} #{release_path}/user.html"
+  end
+end
+end
+after "deploy:finalize_update", "twitter:user_image" 
