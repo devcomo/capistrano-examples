9fa8cad97da477a704db78b4dc0157ceb7deccef
James R. Carr <james.r.carr@gmail.com>
2012-07-17 23:43:26 -0500

    updated h1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,16 @@
 set :application, "awesome-static"
-set :repository,  ""
+set :repository,  "git@github.com:jamescarr/capistrano-example.git"
 set :site, "my.example.com"
 
 set :scm, :git
+set :user, 'www-deploy'
+set :deploy_to,   "/var/www/"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+ssh_options[:forward_agent] = true
+# use MY ssh public key for github so I dont have to put it on the server!
 
 role :web, site                      # Your HTTP server, Apache/etc
 role :app, site                      # Your HTTP server, Apache/etc
--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
 # Static Site
 
 Capistrano example for a static site.
+
+## Steps
+* ssh-copy-id www-deploy@my.example.com
+* add the following to ~/.ssh/config on your machine
+
+```ssh
+Host my.example.com
+  ForwardAgent yes
+
+```
+
+* you may need to ssh in and run ssh -T git@github.com and accept the
+  key
+* cap deploy:setup
+* cap deploy
+* wget http://my.example.com | cat
+* profit
